815df44b7292b492201d2f79269e04aabb270659
darshshah <darshshah1996@gmail.com>
2018-06-20 13:30:15 +0530

    Initial Commit

--- TemperatureGraph.py
+++ TemperatureGraph.py
@@ -1,7 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
 
-data = np.genfromtxt("Gy.csv",delimiter=",")
+style.use("fivethirtyeight")
 
-plt.plot(data)
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+
+
+def animate(i):
+    graph_data = open("Gy.txt","r").read()
+    lines = graph_data.split('\n')
+    xs=[]
+    ys=[]
+
+    for line in lines:
+        if len(line) >1:
+            x,y=line.split(',')
+            xs.append(x)
+            ys.append(y)
+
+    ax1.clear()
+    ax1.plot(xs,ys)
+
+
+ani = animation.FuncAnimation(fig, animate, interval=1000)
 plt.show()
