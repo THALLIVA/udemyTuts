6cde6c0be7fe026c9a6824aaa0701e8d2c638fdc
darshshah <darshshah1996@gmail.com>
2018-06-24 00:06:10 +0530

    Initial Commit

--- AWS.py
+++ AWS.py
@@ -1,10 +1,33 @@
 import boto3
+import paho.mqtt.client as mqtt
 
-client = boto3.client("sns","us-east-1")
+mqttc = mqtt.Client()
 
-phone =input("Enter Phone No:")
-EndMsg = input("Enter Msg:")
+def SendMsg():
+    client = boto3.client("sns", "us-east-1")
+    phone = "+919702007220"
+    phone1 = "+919870533756"
 
-Msg = client.publish(PhoneNumber= phone,Message = EndMsg)
 
-print(Msg)
+    EndMsg = "High Temp         Send Using AWS SNS"
+    Msg = client.publish(PhoneNumber=phone, Message=EndMsg)
+    Msg1 = client.publish(PhoneNumber=phone1, Message=EndMsg)
+
+    print(Msg)
+
+
+def on_message(mosq, obj, msg):
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+    if(int(msg.payload) > 30):
+        SendMsg()
+
+
+
+mqttc.on_message = on_message
+mqttc.connect("192.168.0.101", 1883, 60)
+mqttc.subscribe("test", 0)
+mqttc.loop_forever()
+
+
+
+
