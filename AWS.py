534bd0afae5da97e4fbf68e0459e171c22393465
darshshah <darshshah1996@gmail.com>
2018-06-23 23:38:43 +0530

    Initial Commit

--- AWS.py
+++ AWS.py
@@ -3,7 +3,7 @@ import boto3
 client = boto3.client("sns","us-east-1")
 
 phone =input("Enter Phone No:")
-EndMsg = input("Enter Msg")
+EndMsg = input("Enter Msg:")
 
 Msg = client.publish(PhoneNumber= phone,Message = EndMsg)
 
